d668f8d8ecfaac3ecb539bbe5b7b35a1033c35a6
Megyeri László <36531936+Mlaszlo95@users.noreply.github.com>
2018-10-22 21:50:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 //Alapértelmezett beállitások
 
 const TOKEN = process.env.BOT_TOKEN;
-//const admins = [process.env.ADMIN_KEY1,process.env.ADMIN_KEY2,process.env.ADMIN_KEY3];
+const admins;
 
 
 
